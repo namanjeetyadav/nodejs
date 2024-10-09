bc765585660049ac81219800d20e398806a25ec9
namanjeet <namanshuttler@gmail.com>
2024-10-09 23:40:45 +0530

    09.10.24

--- index2.js
+++ index2.js
@@ -1,3 +1,12 @@
-const fs = require("fs")
+// Challenge
+// 1. Create a folder name CRUD
+// 2. Create a file in it named bio.txt and data into it.
+// 3. Add more data into the file at the end of the existing data
+// 4. Read the data without getting the buffer data at first 
+// 5. Rename the file to mybio.txt
+// 6. Now delete both file and folder
 
-fs.mkdir("Naman")
+const fs = require("fs");
+// fs.mkdirSync("CRUD");
+
+fs.writeFileSync("crud/bio.txt", "Hello")
