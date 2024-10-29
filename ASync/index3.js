999e4bc1d50eca47fa018706eaed5bb403f6e433
namanjeet <namanshuttler@gmail.com>
2024-10-29 23:49:25 +0530

    29.10.24

--- ASync/index3.js
+++ ASync/index3.js
@@ -6,4 +6,15 @@ fs.writeFile("read.txt", "Today is awesome",
     }
 )
 
-// 
+// We pass them a function as an argument - a callback
+// that gets called when task completes
+// The callback has an argument that tells you whether the
+// operation completed sucessfully.
+// Now we need to say what to do when fs.writeFile has completed
+// (even id it's nothing), and start checking for errors.
+
+fs.appendFile("read.txt", "Please add this data to file", 
+(err)=>{
+    console.log("Data added succesfully");
+});
+
