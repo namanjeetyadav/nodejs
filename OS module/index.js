a44d7ee0a3849efd0b733f967894e51d1963b58e
namanjeet <namanshuttler@gmail.com>
2024-11-05 13:51:53 +0530

    05.11.24

--- OS module/index.js
+++ OS module/index.js
@@ -1,5 +1,11 @@
 const os = require("os")
 
+console.log(os.arch());  //Print the architecture 
+console.log(os.hostname()); //Print the hostname 
+console.log(os.platform())   //Print the platform
+console.log(os.tmpdir());  //Print the temprary directory
+console.log(os.type());  //Print the type
+
 const freeMemory = os.freemem();
 console.log(freeMemory/1024/1024/1024);
 
