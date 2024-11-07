d23a3cc27068252591c01305130c79415871e535
namanjeet <namanshuttler@gmail.com>
2024-11-07 19:55:45 +0530

    07.11.24

---
+++ Import_Export/index.js
@@ -0,0 +1,10 @@
+// const Module = require("./oper")
+// console.log(Module.add1(10,5))
+// console.log(Module.sub1(10,5))
+// console.log(Module.multi1(10,5))
+
+const {add, sub, multi, div} = require("./oper")
+console.log(add(10,5))
+console.log(sub(10,5))
+console.log(multi(10,5))
+console.log(div(10,5))
---
+++ Import_Export/oper.js
@@ -0,0 +1,21 @@
+const add = (a,b) =>{
+ return a + b
+} 
+
+const sub = (c,d)=>{
+    return c - d
+}
+const multi = (c,d)=>{
+    return c * d
+}
+const div = (c,d)=>{
+    return c / d
+}
+
+
+// module.exports.add1 = add;
+// module.exports.sub1 = sub;
+// module.exports.multi1 = multi;
+// 
+
+module.exports = {add, sub, multi, div}
