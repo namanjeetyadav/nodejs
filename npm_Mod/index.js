3d95dd758afd33ccad25d3f66a704abf3c6b6419
namanjeet <namanshuttler@gmail.com>
2024-11-11 00:13:26 +0530

    11.11.24

--- npm_Mod/index.js
+++ npm_Mod/index.js
@@ -1,3 +1,9 @@
 const chalk = require("chalk");
+const validator = require("validator")
 
-console.log(chalk.blue.bold("Hello"))
+// console.log(chalk.blue.bold("Hello"))
+
+const res = validator.isEmail("naman@gmail.com")
+
+// console.log(res);
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
