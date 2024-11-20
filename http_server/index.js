f84bdedeafa7732307a2f402b3b09f3ccda91d4e
namanjeet <namanshuttler@gmail.com>
2024-11-20 14:45:20 +0530

    20.11.24

--- http_server/index.js
+++ http_server/index.js
@@ -1,6 +1,21 @@
 const http = require("http")
+
 const server = http.createServer((req, res) => {
-    res.end("Hello from the other side naman.")
+   if(req.url == "/"){
+   res.end("Welcome from HOME")
+   }
+   else if (req.url == "/about"){
+    res.end("Welcome from ABOUT")
+   }
+   else if (req.url == "/contact"){
+    res.end("Welcome from CONTACT")
+   }
+   else {
+    res.writeHead(404, {"Content-type" : "text-html"});
+    res.end("<h1>404 Page Not Found</h1>")
+   }
+
+
 });
 
 server.listen(8000, "127.0.0.1", () => {
