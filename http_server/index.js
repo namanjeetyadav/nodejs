b9852a30b92f88d0c69ec86772ed693e8a612c85
namanjeet <namanshuttler@gmail.com>
2024-12-08 23:55:31 +0530

    08.12.24

--- http_server/index.js
+++ http_server/index.js
@@ -1,19 +1,22 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-   if(req.url == "/"){
-   res.end("Welcome from HOME")
-   }
-   else if (req.url == "/about"){
-    res.end("Welcome from ABOUT")
-   }
-   else if (req.url == "/contact"){
-    res.end("Welcome from CONTACT")
-   }
-   else {
-    res.writeHead(404, {"Content-type" : "text-html"});
-    res.end('<h1 style="color: Grey;"> 404 Page Not Found</h1>')
-   }
+    if (req.url == "/") {
+        res.end("Welcome from HOME")
+    }
+    else if (req.url == "/about") {
+        res.end("Welcome from ABOUT")
+    }
+    else if (req.url == "/contact") {
+        res.end("Welcome from CONTACT")
+    }
+    else if (req.url == "/userapi") {
+        res.end("Hello from user API side")
+    }
+    else {
+        res.writeHead(404, { "Content-type": "text-html" });
+        res.end('<h1 style="color: Grey;"> 404 Page Not Found</h1>')
+    }
 });
 
 server.listen(8000, "127.0.0.1", () => {
