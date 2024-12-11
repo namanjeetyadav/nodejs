aca7ece2f9753d512c1c431a546f7aa05134b9c1
namanjeet <namanshuttler@gmail.com>
2024-12-11 23:18:23 +0530

    11.12.24

--- http_server/index.js
+++ http_server/index.js
@@ -2,6 +2,12 @@ const http = require("http")
 const fs = require("fs")
 
 const server = http.createServer((req, res) => {
+
+    const data = fs.readFileSync(`${__dirname}/userApi/userApi.json`, "utf-8")
+    const objData = JSON.parse(data)
+    
+
+    
     if (req.url == "/") {
         res.end("Welcome from HOME")
     }
@@ -12,18 +18,16 @@ const server = http.createServer((req, res) => {
         res.end("Welcome from CONTACT")
     }
     else if (req.url == "/userapi") {
-        fs.readFile(`${__dirname}/userApi/userApi.json`, "utf-8", (err, data) => {
-            console.log(data);
-            res.end(data);
-
-        })
+        res.writeHead(200, {"content-type": "application/json"});
+        res.end(objData[0].name);
+        
     }
     else {
-        res.writeHead(404, { "Content-type": "text-html" });
+        res.writeHead(404, { "Content-type": "text/html" });
         res.end('<h1 style="color: Grey;"> 404 Page Not Found</h1>')
     }
 });
 
 server.listen(8000, "127.0.0.1", () => {
     console.log("Listening to server port no 8000");
-})
+});
