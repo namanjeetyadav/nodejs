58a480ae62d469b08aef14c576d773c2505d5f1f
namanjeet <namanshuttler@gmail.com>
2024-12-18 13:27:05 +0530

    18.12.24

--- Event module/index.js
+++ Event module/index.js
@@ -2,20 +2,30 @@
 // Node.js has a build-in module, called "Events",
 // Where you can create, fire and listen for- your own events.
 
-const EventEmitter = require("events")
+// const EventEmitter = require("events")
 
+// const event = new EventEmitter();
+
+// event.on("sayMyName", ()=>{
+//     console.log("Your name is Naman")
+// });
+
+// event.on("sayMyName", ()=>{
+//     console.log("My middle name is Jeet")
+// });
+
+// event.on("sayMyName", ()=>{
+//     console.log("My last name is Yadav")
+// })
+
+// event.emit("sayMyName")
+
+const EventEmitter = require("events")
 const event = new EventEmitter();
 
-event.on("sayMyName", ()=>{
-    console.log("Your name is Naman")
-});
-
-event.on("sayMyName", ()=>{
-    console.log("My middle name is Jeet")
-});
-
-event.on("sayMyName", ()=>{
-    console.log("My last name is Yadav")
+event.on("checkPage", (sc, msg)=>{
+    console.log(`status code ${sc} and the page is ${msg}`);
 })
 
-event.emit("sayMyName")
+event.emit("checkPage", 200, "ok");
+
