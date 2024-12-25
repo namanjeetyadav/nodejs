26638959c72606c6fcd0a12934ebe1f16a779a0c
namanjeet <namanshuttler@gmail.com>
2024-12-25 18:03:39 +0530

    24.12.24

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -4,11 +4,7 @@ const http =  require("http")
 const server = http.createServer();
 
 server.on("request", (req, res) =>{
-    // fs.readFile("input.txt", (err, data) =>{
-    //     if(err) return console.error(err);
-    //     res.end(data.toString());
-    // })
-
+   
     const rstream = fs.createReadStream("input.txt")
     rstream.on("data", (chunkdata)=>{
         res.write(chunkdata);
