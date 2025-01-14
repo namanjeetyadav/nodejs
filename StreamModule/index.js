2cf915d8e9c8192da8a99f5d21ec8abfd90f07c1
namanjeet <namanshuttler@gmail.com>
2025-01-14 10:57:35 +0530

    14.01.25

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -5,17 +5,22 @@ const server = http.createServer();
 
 server.on("request", (req, res) =>{
    
-    const rstream = fs.createReadStream("input.txt")
-    rstream.on("data", (chunkdata)=>{
-        res.write(chunkdata);
-    })
-    rstream.on("end", ()=>{
-        res.end();
-    });
-    rstream.on("error", (err)=>{
-        console.log(err);
-        res.end("File not found")
-    })
+    // const rstream = fs.createReadStream("input.txt")
+    // console.log(rstream);
+    // rstream.on("data", (chunkdata)=>{
+    //     res.write(chunkdata);
+    // })
+    // rstream.on("end", ()=>{
+    //     res.end();
+    // });
+    // rstream.on("error", (err)=>{
+    //     console.log(err);
+    //     res.end("File not found")
+    // })
+
+    // 2rd way by using pipe method
+    const rstream = fs.createReadStream("input.txt");
+    rstream.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1")
