ba599627af5fed8a1a7a0eb8aedddf4e2f534e2b
namanjeet <namanshuttler@gmail.com>
2025-01-21 14:31:47 +0530

    21.01.25

--- Mail/app.js
+++ Mail/app.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
+let PORT = 5000;
 
-app.get("/")
+app.get("/", (req, res) => {
+    res.send("I am a server")
+});
+
+const start = async () => {
+    try {
+        app.listen(5000, () => {
+            console.log(`I am live in port 5000 ${PORT}`)
+        });
+    }catch (error) {}
+};
+
+start()
